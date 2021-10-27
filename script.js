a73ccc279c68aca88f62093d99f5b68714800f75
FechL <fawwasaliy11@gmail.com>
2021-10-28 00:43:48 +0700

    update tictactoe

--- script.js
+++ script.js
@@ -1,150 +1,165 @@
-const kotak = document.querySelectorAll('.kotak');
+// DOM
+const box = document.querySelectorAll('.box');
 const gameOver = document.querySelector('.game-over');
 const xScore = document.querySelector('.x-score');
 const oScore = document.querySelector('.o-score');
-const resetGame = document.querySelector('.reset-game');
-const resetScore = document.querySelector('.reset-score');
-// const ktkWin = document.querySelector('.ktkWin');
-// const ktk1 = document.querySelector('.ktk1');
-// const ktk1 = document.querySelector('.ktk2');
-// const ktk1 = document.querySelector('.ktk3');
-// const ktk1 = document.querySelector('.ktk4');
-// const ktk1 = document.querySelector('.ktk5');
-// const ktk1 = document.querySelector('.ktk6');
-// const ktk1 = document.querySelector('.ktk7');
-// const ktk1 = document.querySelector('.ktk8');
-// const ktk1 = document.querySelector('.ktk9');
-let toggle = 'x';
-kotak.forEach(m => m.addEventListener('click', function () {
+const clear = document.querySelector('.clear');
+const reset = document.querySelector('.reset');
 
-    // toggle
+// Action
+let toggle = 'x';
+let tgl = 0;
+xScore.style.color = "rgb(23, 218, 49)";
+oScore.style.color = "rgb(23, 156, 36)";
+
+box.forEach(m => m.addEventListener('click', function () {
     if (this.classList.contains("x") || this.classList.contains("o")) {
-        
     } else if (toggle == "x") {
         this.classList.add("x");
         this.innerHTML = 'x';
+        oScore.style.color = "rgb(23, 218, 49)";
+        xScore.style.color = "rgb(23, 156, 36)";
         toggle = "o";
     } else if (toggle == "o") {
         this.classList.add("o");
         this.innerHTML = 'o';
+        oScore.style.color = "rgb(23, 156, 36)";
+        xScore.style.color = "rgb(23, 218, 49)";
         toggle = "x";
     }
     win();
 }));
 
-resetGame.addEventListener('click', function () {
-    reset();
+clear.addEventListener('click', function () {
+    tgl++;
+    restart();
 })
-resetScore.addEventListener('click', function () {
+
+reset.addEventListener('click', function () {
     xPt = 0;
     oPt = 0;
     xScore.innerHTML = `X : ` + xPt;
     oScore.innerHTML = `O : ` + oPt;
-    reset();
+    toggle = "x";
+    tgl = 0;
+    restart();
 })
 
-// skor
-    let xPt = 0;
-    let oPt = 0;
+let xPt = 0;
+let oPt = 0;
 
-// function
+// Function
 function win() {
-    // ketika x menang
-    if (kotak[0].classList.contains("x") && kotak[1].classList.contains("x") && kotak[2].classList.contains("x")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[1].classList.add("ktkWin");
-        kotak[2].classList.add("ktkWin");
+    if (box[0].classList.contains("x") && box[1].classList.contains("x") && box[2].classList.contains("x")) {
+        box[0].classList.add("box-win");
+        box[1].classList.add("box-win");
+        box[2].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[3].classList.contains("x") && kotak[4].classList.contains("x") && kotak[5].classList.contains("x")) {
-        kotak[3].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[5].classList.add("ktkWin");
+    if (box[3].classList.contains("x") && box[4].classList.contains("x") && box[5].classList.contains("x")) {
+        box[3].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[5].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[6].classList.contains("x") && kotak[7].classList.contains("x") && kotak[8].classList.contains("x")) {
-        kotak[6].classList.add("ktkWin");
-        kotak[7].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[6].classList.contains("x") && box[7].classList.contains("x") && box[8].classList.contains("x")) {
+        box[6].classList.add("box-win");
+        box[7].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[0].classList.contains("x") && kotak[3].classList.contains("x") && kotak[6].classList.contains("x")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[3].classList.add("ktkWin");
-        kotak[6].classList.add("ktkWin");
+    if (box[0].classList.contains("x") && box[3].classList.contains("x") && box[6].classList.contains("x")) {
+        box[0].classList.add("box-win");
+        box[3].classList.add("box-win");
+        box[6].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[1].classList.contains("x") && kotak[4].classList.contains("x") && kotak[7].classList.contains("x")) {
-        kotak[1].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[7].classList.add("ktkWin");
+    if (box[1].classList.contains("x") && box[4].classList.contains("x") && box[7].classList.contains("x")) {
+        box[1].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[7].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[2].classList.contains("x") && kotak[5].classList.contains("x") && kotak[8].classList.contains("x")) {
-        kotak[2].classList.add("ktkWin");
-        kotak[5].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[2].classList.contains("x") && box[5].classList.contains("x") && box[8].classList.contains("x")) {
+        box[2].classList.add("box-win");
+        box[5].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[0].classList.contains("x") && kotak[4].classList.contains("x") && kotak[8].classList.contains("x")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[0].classList.contains("x") && box[4].classList.contains("x") && box[8].classList.contains("x")) {
+        box[0].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    if (kotak[2].classList.contains("x") && kotak[4].classList.contains("x") && kotak[6].classList.contains("x")) {
-        kotak[2].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[6].classList.add("ktkWin");
+    if (box[2].classList.contains("x") && box[4].classList.contains("x") && box[6].classList.contains("x")) {
+        box[2].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[6].classList.add("box-win");
+        highlightScore();
         scoreX();
     }
-    // ketika o menang
-    if (kotak[0].classList.contains("o") && kotak[1].classList.contains("o") && kotak[2].classList.contains("o")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[1].classList.add("ktkWin");
-        kotak[2].classList.add("ktkWin");
+    if (box[0].classList.contains("o") && box[1].classList.contains("o") && box[2].classList.contains("o")) {
+        box[0].classList.add("box-win");
+        box[1].classList.add("box-win");
+        box[2].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[3].classList.contains("o") && kotak[4].classList.contains("o") && kotak[5].classList.contains("o")) {
-        kotak[3].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[5].classList.add("ktkWin");
+    if (box[3].classList.contains("o") && box[4].classList.contains("o") && box[5].classList.contains("o")) {
+        box[3].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[5].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[6].classList.contains("o") && kotak[7].classList.contains("o") && kotak[8].classList.contains("o")) {
-        kotak[6].classList.add("ktkWin");
-        kotak[7].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[6].classList.contains("o") && box[7].classList.contains("o") && box[8].classList.contains("o")) {
+        box[6].classList.add("box-win");
+        box[7].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[0].classList.contains("o") && kotak[3].classList.contains("o") && kotak[6].classList.contains("o")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[3].classList.add("ktkWin");
-        kotak[6].classList.add("ktkWin");
+    if (box[0].classList.contains("o") && box[3].classList.contains("o") && box[6].classList.contains("o")) {
+        box[0].classList.add("box-win");
+        box[3].classList.add("box-win");
+        box[6].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[1].classList.contains("o") && kotak[4].classList.contains("o") && kotak[7].classList.contains("o")) {
-        kotak[1].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[7].classList.add("ktkWin");
+    if (box[1].classList.contains("o") && box[4].classList.contains("o") && box[7].classList.contains("o")) {
+        box[1].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[7].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[2].classList.contains("o") && kotak[5].classList.contains("o") && kotak[8].classList.contains("o")) {
-        kotak[2].classList.add("ktkWin");
-        kotak[5].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[2].classList.contains("o") && box[5].classList.contains("o") && box[8].classList.contains("o")) {
+        box[2].classList.add("box-win");
+        box[5].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[0].classList.contains("o") && kotak[4].classList.contains("o") && kotak[8].classList.contains("o")) {
-        kotak[0].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[8].classList.add("ktkWin");
+    if (box[0].classList.contains("o") && box[4].classList.contains("o") && box[8].classList.contains("o")) {
+        box[0].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[8].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
-    if (kotak[2].classList.contains("o") && kotak[4].classList.contains("o") && kotak[6].classList.contains("o")) {
-        kotak[2].classList.add("ktkWin");
-        kotak[4].classList.add("ktkWin");
-        kotak[6].classList.add("ktkWin");
+    if (box[2].classList.contains("o") && box[4].classList.contains("o") && box[6].classList.contains("o")) {
+        box[2].classList.add("box-win");
+        box[4].classList.add("box-win");
+        box[6].classList.add("box-win");
+        highlightScore();
         scoreO();
     }
 }
@@ -153,21 +168,39 @@ function scoreX() {
     gameOver.classList.remove("none");
     xPt += 1;
     xScore.innerHTML = `X : ` + xPt;
+    toggle = 'o';
 }
 
 function scoreO() {
     gameOver.classList.remove("none");
     oPt += 1;
     oScore.innerHTML = `O : ` + oPt;
+    toggle = 'x';
 }
 
-function reset() {
-    kotak.forEach(m => {
+function highlightScore() {
+    xScore.style.color = "rgb(23, 218, 49)";
+    oScore.style.color = "rgb(23, 218, 49)";
+}
+
+function restart() {
+    box.forEach(m => {
         m.classList.remove("x");
         m.classList.remove("o");
-        m.classList.remove("ktkWin");
+        m.classList.remove("box-win");
         m.innerHTML = '';
-        toggle = 'x';
+        if (toggle == "x") {
+            xScore.style.color = "rgb(23, 218, 49)";
+            oScore.style.color = "rgb(23, 156, 36)"; 
+        } else if (toggle == "o") {
+            oScore.style.color = "rgb(23, 218, 49)";
+            xScore.style.color = "rgb(23, 156, 36)"; 
+        }
+        if (tgl % 2 == 0) {
+            toggle = 'x';
+        } else if (tgl % 2 == 1) {
+            toggle = 'o';
+        }
     })
     gameOver.classList.add("none");
 }
